030494840ef8c849cdaa20926d4f6e26025489ce
djon125 <ross.dion9027@gmail.com>
2022-06-14 22:52:46 -0500

    Working on selecting a random object in weapon class

--- weapon.py
+++ weapon.py
@@ -1,13 +1,16 @@
-
+import random
 class Weapon:
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
 
-    
 
 
 gun = Weapon('glock', 20)
 sword = Weapon('excalibur', 30)
 light_saber = Weapon('light saber', 40)
 
+Weapons = [gun, sword, light_saber]
+print(type(Weapons))
+w_one = random.choice(Weapons)
+print(w_one)
